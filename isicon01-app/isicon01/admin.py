4312a959259e14b99eadcf84ef8880f56c4a09f7
obara <obara.yuki@tis.co.jp>
2018-11-28 18:34:49 +0900

    modify gotty url

--- isicon01-app/isicon01/admin.py
+++ isicon01-app/isicon01/admin.py
@@ -6,27 +6,19 @@ from isicon01 import app
 from flask import Flask, render_template, request, json
 import datetime
 import sqlite3
+import random
 
 
 dbpath = 'data.db'
 
 @app.route('/admin')
 def admin():
-    # db connection
-    conn = sqlite3.connect(dbpath, detect_types=sqlite3.PARSE_DECLTYPES|sqlite3.PARSE_COLNAMES)
-    sqlite3.dbapi2.converters['DATETIME'] = sqlite3.dbapi2.converters['TIMESTAMP']
+    conn = sqlite3.connect(dbpath)
     c = conn.cursor()
 
     try:
-        #c.execute("drop table if exists server")
-        c.execute("create table if not exists server (num integer primary key, ip text, use text)")
-        #c.execute("insert into server values (?, ?, ?)", (1, "xxx.xxx.xxx.xxx", 0))
-
-        ret = c.execute("select * from server;")
+        c.execute("select * from server;")
         servers = c.fetchall()
-        for row in ret:
-            print(row)
-
     except sqlite3.Error as e:
         print('sqlite3 error: ', e.args[0])
 
@@ -42,19 +34,16 @@ def admin():
 
 @app.route('/admin/add', methods=['POST'])
 def add():
-    # db connection
-    conn = sqlite3.connect(dbpath, detect_types=sqlite3.PARSE_DECLTYPES|sqlite3.PARSE_COLNAMES)
-    sqlite3.dbapi2.converters['DATETIME'] = sqlite3.dbapi2.converters['TIMESTAMP']
+    ip = request.form['ip']
+    port = random.randint(10000,19999)
+
+    conn = sqlite3.connect(dbpath)
     c = conn.cursor()
 
     try:
-        c.execute("insert into server(ip) values(?)", (request.form['ip'],))
-
-        ret = c.execute("select * from server;")
+        c.execute("insert into server(ip, port) values(?, ?)", (ip, port))
+        c.execute("select * from server;")
         servers = c.fetchall()
-        for row in ret:
-            print(row)
-
     except sqlite3.Error as e:
         print('sqlite3 error: ', e.args[0])
 
@@ -70,26 +59,52 @@ def add():
 
 @app.route('/admin/<int:num>/del', methods=['POST'])
 def delete(num):
-    # db connection
-    conn = sqlite3.connect(dbpath, detect_types=sqlite3.PARSE_DECLTYPES|sqlite3.PARSE_COLNAMES)
-    sqlite3.dbapi2.converters['DATETIME'] = sqlite3.dbapi2.converters['TIMESTAMP']
+    conn = sqlite3.connect(dbpath)
     c = conn.cursor()
 
     try:
         c.execute("delete from server where num = ?", (num,))
-
-        ret = c.execute("select * from server;")
+        c.execute("select * from server;")
         servers = c.fetchall()
-        for row in ret:
-            print(row)
-        print(servers)
     except sqlite3.Error as e:
         print('sqlite3 error: ', e.args[0])
 
     conn.commit()
     conn.close()
 
+    print(servers)
+
     return render_template(
         'admin.html',
         servers = servers,
     )
+
+
+@app.route('/admin/createTable', methods=['POST'])
+def createTable():
+    conn = sqlite3.connect(dbpath)
+    c = conn.cursor()
+    try:
+        c.execute("create table if not exists server (num integer primary key, ip text, port integer, use text)")
+    except sqlite3.Error as e:
+        print('sqlite3 error: ', e.args[0])
+    conn.commit()
+    conn.close()
+    return render_template(
+        'admin.html',
+    )
+
+@app.route('/admin/deleteTable', methods=['POST'])
+def deleteTable():
+    conn = sqlite3.connect(dbpath)
+    c = conn.cursor()
+    try:
+        c.execute("drop table if exists server")
+    except sqlite3.Error as e:
+        print('sqlite3 error: ', e.args[0])
+    conn.commit()
+    conn.close()
+    return render_template(
+        'admin.html',
+    )
+
--- isicon01-app/isicon01/views.py
+++ isicon01-app/isicon01/views.py
@@ -12,6 +12,9 @@ import subprocess
 dbpath = 'data.db'
 etime = 120
 
+
+
+
 @app.route('/')
 def hello():
     return render_template(
@@ -21,25 +24,14 @@ def hello():
 @app.route('/start', methods=['POST'])
 def start():
     print(request.form)
-
+    
     # param set
     userid = request.form['id']
     starttime = datetime.datetime.now()
-    finishtime = starttime + datetime.timedelta(minutes=etime)
-    endtime = finishtime
+    endtime = starttime + datetime.timedelta(minutes=etime)
+    finishtime = endtime
 
-    # gotty connection
-    client = '192.168.10.2'
-    try:
-        args = [
-            'gotty',
-            '-w',
-            'ssh',
-            client,
-        ]
-        command = subprocess.Popen(args)
-    except:
-        pass
+    gotty_url = create_gotty_connection()
 
     # db connection
     conn = sqlite3.connect(dbpath, detect_types=sqlite3.PARSE_DECLTYPES|sqlite3.PARSE_COLNAMES)
@@ -48,8 +40,8 @@ def start():
 
     try:
         c.execute("drop table if exists exam")
-        c.execute("create table if not exists exam (userid text, starttime datetime, endtime datetime)")
-        c.execute("insert into exam values (?, ?, ?)", (userid, starttime, endtime))
+        c.execute("create table if not exists exam (userid text, starttime datetime, endtime datetime, finishtime datetime)")
+        c.execute("insert into exam values (?, ?, ?, ?)", (userid, starttime, endtime, finishtime))
 
         ret = c.execute("select * from exam;")
         for row in ret:
@@ -66,7 +58,43 @@ def start():
         id = userid,
         starttime = starttime,
         endtime = endtime,
-        gotty = 'http://192.168.175.27:8080/',
+        gotty = gotty_url,
     )
+
+
+
+# create gotty connection
+def create_gotty_connection():
+    conn = sqlite3.connect(dbpath)
+    c = conn.cursor()
+    try:
+        c.execute("SELECT * FROM server WHERE use IS NULL;")
+        servers = c.fetchall()
+        for server in servers:
+            num = server[0]
+            ip = server[1]
+            port = server[2]
+            if server[3] is None:
+                c.execute("UPDATE server SET use = 'use' WHERE num = ?", (num,))
+                gotty_url = 'http://' + '192.168.175.27' + ':' + str(port) + '/'
+                break
+    except sqlite3.Error as e:
+        print('sqlite3 error: ', e.args[0])
+    conn.commit()
+    conn.close()
+
+    try:
+        args = [
+            'gotty',
+            '-w',
+            'ssh',
+            ip,
+        ]
+        print(args)
+        command = subprocess.Popen(args)
+    except:
+        pass
+
+    return gotty_url
 
 
